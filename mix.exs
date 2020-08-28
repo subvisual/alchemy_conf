43b0f00190e078213a66e11b4a7ef5299427d70c
Fernando Mendes <fernando@mendes.codes>
2020-08-28 18:44:45 +0100

    v0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AlchemyConf.MixProject do
   def project do
     [
       app: :alchemy_conf,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
