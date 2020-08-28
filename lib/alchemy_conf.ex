e9dae77a92aadf59457313c5e6f639d5c79f9937
Fernando Mendes <fernando@mendes.codes>
2020-08-28 10:37:25 +0100

    :sunrise:

---
+++ lib/alchemy_conf.ex
@@ -0,0 +1,18 @@
+defmodule AlchemyConf do
+  @moduledoc """
+  Documentation for `AlchemyConf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AlchemyConf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alchemy_conf_test.exs
@@ -0,0 +1,8 @@
+defmodule AlchemyConfTest do
+  use ExUnit.Case
+  doctest AlchemyConf
+
+  test "greets the world" do
+    assert AlchemyConf.hello() == :world
+  end
+end
